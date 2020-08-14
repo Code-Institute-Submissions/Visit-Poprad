256bb367583a05743c50e1ec0f489ffd0a44489f
JanaBuckova <janabuckova4@gmail.com>
2020-08-14 21:58:45 +0000

    Added Google Maps API, intitialized map

---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+function initMap() {
+        let mymap = new google.maps.Map(document.getElementById("mymap"), {
+        zoom: 15,
+        center: {
+            lat: 49.054001,
+            lng: 20.295059
+        },
+    });
+}
