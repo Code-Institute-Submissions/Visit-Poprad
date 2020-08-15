f56f4627e0cf49e618c55767c8c7c2b71c072801
JanaBuckova <janabuckova4@gmail.com>
2020-08-15 15:15:52 +0000

    Added custom marker img, added custom marker code

--- assets/js/map.js
+++ assets/js/map.js
@@ -5,10 +5,20 @@ function initMap() {
             lat: 49.054001,
             lng: 20.295059},
     });
+
+//Marker resizing frm IFD leflet by Tim from CI and Stackoverflow    
+    let pattiserie = {
+    url: 'assets/images/marker_icons/pattiserie.png', // url
+    scaledSize: new google.maps.Size(40, 40), // scaled size
+    origin: new google.maps.Point(0,0), // origin
+    anchor: new google.maps.Point(0, 0) // anchor
+};
+
 // Added first marker - to test     
     let marker = new google.maps.Marker({
-        position:{lat:49.055575, lng:20.301116},
-        map: mymap 
-    })
+        position:{lat:49.055575, lng:20.301116}, //Palacinkáreň u Priateľov 
+        map: mymap,
+        icon: pattiserie
+    });
 }
 
