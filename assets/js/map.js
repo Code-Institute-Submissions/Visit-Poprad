b34fb5407ba80f6f7b122d53832e8a75bfa52f35
JanaBuckova <janabuckova4@gmail.com>
2020-08-15 16:50:43 +0000

    Added more custom markers and corrected spelling

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,19 +6,42 @@ function initMap() {
             lng: 20.295059},
     });
 
-//Marker resizing frm IFD leflet by Tim from CI and Stackoverflow    
-    let pattiserie = {
-    url: 'assets/images/marker_icons/pattiserie.png', // url
+//Marker Icons,     
+//Marker resizing from IFD leflet by Tim from CI and Stackoverflow    
+    let patisserie = {
+    url: 'assets/images/marker_icons/patisserie.png', // url
     scaledSize: new google.maps.Size(40, 40), // scaled size
     origin: new google.maps.Point(0,0), // origin
     anchor: new google.maps.Point(0, 0) // anchor
-};
+    };
+
+    let restaurant = {
+    url: 'assets/images/marker_icons/restaurant.png', // url
+    scaledSize: new google.maps.Size(40, 40), // scaled size
+    origin: new google.maps.Point(0,0), // origin
+    anchor: new google.maps.Point(0, 0) // anchor    
+    }
+
+    let bar = {
+    url: 'assets/images/marker_icons/bar.png', // url
+    scaledSize: new google.maps.Size(40, 40), // scaled size
+    origin: new google.maps.Point(0,0), // origin
+    anchor: new google.maps.Point(0, 0) // anchor    
+    }
+
+    let attraction = {
+    url: 'assets/images/marker_icons/attraction.png', // url
+    scaledSize: new google.maps.Size(40, 40), // scaled size
+    origin: new google.maps.Point(0,0), // origin
+    anchor: new google.maps.Point(0, 0) // anchor    
+    }
 
 // Added first marker - to test     
     let marker = new google.maps.Marker({
         position:{lat:49.055575, lng:20.301116}, //Palacinkáreň u Priateľov 
         map: mymap,
-        icon: pattiserie
+        icon: patisserie
     });
 }
+  
 
