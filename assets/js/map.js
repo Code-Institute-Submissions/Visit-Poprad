90933af5a7d270a6dae46199612e661dbb59e22a
JanaBuckova <janabuckova4@gmail.com>
2020-08-15 18:38:23 +0000

    Cleaned the marker icons code, resize not working

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,42 +6,61 @@ function initMap() {
             lng: 20.295059},
     });
 
-//Marker Icons,     
-//Marker resizing from IFD leflet by Tim from CI and Stackoverflow    
-    let patisserie = {
-    url: 'assets/images/marker_icons/patisserie.png', // url
-    scaledSize: new google.maps.Size(40, 40), // scaled size
-    origin: new google.maps.Point(0,0), // origin
-    anchor: new google.maps.Point(0, 0) // anchor
-    };
-
-    let restaurant = {
-    url: 'assets/images/marker_icons/restaurant.png', // url
-    scaledSize: new google.maps.Size(40, 40), // scaled size
-    origin: new google.maps.Point(0,0), // origin
-    anchor: new google.maps.Point(0, 0) // anchor    
-    }
-
-    let bar = {
-    url: 'assets/images/marker_icons/bar.png', // url
-    scaledSize: new google.maps.Size(40, 40), // scaled size
-    origin: new google.maps.Point(0,0), // origin
-    anchor: new google.maps.Point(0, 0) // anchor    
-    }
-
-    let attraction = {
-    url: 'assets/images/marker_icons/attraction.png', // url
-    scaledSize: new google.maps.Size(40, 40), // scaled size
-    origin: new google.maps.Point(0,0), // origin
-    anchor: new google.maps.Point(0, 0) // anchor    
-    }
+//Marker icons categories,     
+//Marker resizing from IFD leflet by Tim from CI and Stackoverflow
+//code src Google documentation    
+    let icons = {
+        patisserie: {
+            name: 'Patisseries',
+            icon: 'assets/images/marker_icons/patisserie.png', // url
+            scaledSize: new google.maps.Size(40, 40), // scaled size
+            origin: new google.maps.Point(0,0), // origin
+            anchor: new google.maps.Point(0, 0) // anchor
+        },
+        restaurant: {
+            name: 'Restaurants',
+            icon: 'assets/images/marker_icons/restaurant.png', // url
+            scaledSize: new google.maps.Size(40, 40), // scaled size
+            origin: new google.maps.Point(0,0), // origin
+            anchor: new google.maps.Point(0, 0) // anchor    
+        },
+        bar: {
+            name: 'Bars',
+            icon: 'assets/images/marker_icons/bar.png', // url
+            scaledSize: new google.maps.Size(40, 40), // scaled size
+            origin: new google.maps.Point(0,0), // origin
+            anchor: new google.maps.Point(0, 0) // anchor    
+        },
+        attraction: {
+            name: 'Attractions',
+            icon: 'assets/images/marker_icons/attraction.png', // url
+            scaledSize: new google.maps.Size(40, 40), // scaled size
+            origin: new google.maps.Point(0,0), // origin
+            anchor: new google.maps.Point(0, 0) // anchor    
+        }
+    };   
 
 // Added first marker - to test     
-    let marker = new google.maps.Marker({
+    let locations = [
+        {
         position:{lat:49.055575, lng:20.301116}, //Palacinkáreň u Priateľov 
-        map: mymap,
-        icon: patisserie
-    });
-}
-  
+        type: 'patisserie'
+        },
+        {
+        position:{lat:49.054578, lng:20.299540}, // Cukraren Lucia
+        type: 'bar'
+        }
+    ];
 
+    for (let i = 0; i < locations.length; i++) {
+        addMarker(locations[i]);
+    }
+
+    function addMarker(props) {
+        let marker = new google.maps.Marker({
+            position: props.position,
+            icon: icons[props.type].icon,
+            map: mymap
+        });
+    };
+}    
