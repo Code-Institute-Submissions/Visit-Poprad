7a6e53cd0c5715431f50bd74a8e272c3efc3d7a9
JanaBuckova <janabuckova4@gmail.com>
2020-08-15 13:09:41 +0000

    Added firts map marker for testing

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,14 @@
 function initMap() {
-        let mymap = new google.maps.Map(document.getElementById("mymap"), {
+    let mymap = new google.maps.Map(document.getElementById("mymap"), {
         zoom: 15,
         center: {
             lat: 49.054001,
-            lng: 20.295059
-        },
+            lng: 20.295059},
     });
+// Added first marker - to test     
+    let marker = new google.maps.Marker({
+        position:{lat:49.055575, lng:20.301116},
+        map: mymap 
+    })
 }
+
