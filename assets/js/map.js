a125ff778b3594262ee57fd3d83db2f464ee50b6
JanaBuckova <janabuckova4@gmail.com>
2020-08-16 14:24:11 +0000

    Added: more markers, info windows, moved scripts

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,6 @@
 function initMap() {
     let mymap = new google.maps.Map(document.getElementById("mymap"), {
-        zoom: 15,
+        zoom: 14,
         center: {
             lat: 49.054001,
             lng: 20.295059},
@@ -31,20 +31,103 @@ function initMap() {
         {
         position:{lat:49.065848, lng:20.283341}, // Cukráreň Domenico
         type: 'patisserie',
-        content:"<p>Test</p>"
+        content:"<img src='assets/images/info_windows/domenico_120x100.jpg'><p>Cukráreň Domenico</p>",
         },
         {
         position:{lat:49.054578, lng:20.299540}, // Cukráreň Lucia
+        content:"<img src='assets/images/info_windows/lucia_120x100.jpg'><p>Cukráreň Lucia</p>",
         type: 'patisserie'
         },
         {
         position:{lat:49.046588, lng:20.283674}, // Cukráreň Sonata
+        content:"<img src='assets/images/info_windows/sonata_120x100.jpg'><p>Cukráreň Sonata</p>",
         type: 'patisserie'    
         },
         {
         position:{lat:49.054448, lng:20.301053}, // Sladký sen
+        content:"<img src='assets/images/info_windows/sen_120x100.jpg'><p>Sladký sen</p>",
         type: 'patisserie'    
-        }
+        },
+        {
+        position:{lat:49.053814, lng:20.298154}, // Betka's bakery 
+        content:"<img src='assets/images/info_windows/betka_120x100.jpg'><p>Betka's bakery</p>",
+        type: 'patisserie'    
+        },
+        {
+        position:{lat:49.055444, lng:20.304947}, // Cukráreň-kaviareň Perla
+        content:"<img src='assets/images/info_windows/perla_120x100.jpg'><p>Cukráreň-kaviareň Perla</p>",
+        type: 'patisserie'    
+        },
+        {
+        position:{lat:49.067814, lng:20.279599}, // U septima
+        content:"<img src='assets/images/info_windows/u_septima_120x100.jpg'><p>U septima</p>",
+        type: 'restaurant'    
+        },
+        {
+        position:{lat:49.056806, lng:20.296760}, // Marco
+        content:"<img src='assets/images/info_windows/marco_120x100.png'><p>Marco</p>",
+        type: 'restaurant'    
+        },
+        {
+        position:{lat:49.054981, lng:20.303166}, // Lahodnosti
+        content:"<img src='assets/images/info_windows/lahodnosti_120x100.jpg'><p>Lahodnosti</p>",
+        type: 'restaurant'    
+        },
+        {
+        position:{lat:49.055797, lng:20.301358}, // Hodovňa
+        content:"<img src='assets/images/info_windows/hodovna_120x100.jpg'><p>Hodovňa</p>",
+        type: 'restaurant'    
+        },
+        {
+        position:{lat:49.055257, lng:20.304141}, // Café Razy Restaurant
+        content:"<img src='assets/images/info_windows/crazy_120x100.jpg'><p>Café Razy Restaurant</p>",
+        type: 'restaurant'    
+        },
+        {
+        position:{lat:49.057447, lng:20.300821}, // Haliganda
+        content:"<img src='assets/images/info_windows/haliganda_120x100.jpg'><p>Haliganda</p>",
+        type: 'restaurant'    
+        },
+        {
+        position:{lat:49.066406, lng:20.315812}, // Vino & Tapas
+        content:"<img src='assets/images/info_windows/vinotapas_120x100.jpg'><p>Haliganda</p>",
+        type: 'restaurant'    
+        },
+        {
+        position:{lat:49.054621, lng:20.3032372}, // Beer Spa Tatras
+        content:"<img src='assets/images/info_windows/beer_spa_120x100.jpg'><p>Beer Spa Tatras</p>",
+        type: 'attraction'    
+        },
+        {
+        position:{lat:49.060179, lng:20.307020}, // AquaCity Poprad
+        content:"<img src='assets/images/info_windows/aquacity_120x100.jpg'><p>AquaCity Poprad</p>",
+        type: 'attraction'    
+        },
+        {
+        position:{lat:49.053631, lng:20.297266}, // Podtatranské múzeum
+        content:"<img src='assets/images/info_windows/tatramuseum_120x100.jpg'><p>Tatra Museum in Poprad</p>",
+        type: 'attraction'    
+        },
+        {
+        position:{lat:49.057091, lng:20.275942}, // Woodrock lezecka stena
+        content:"<img src='assets/images/info_windows/woodrock_120x100.jpg'><p>WoodRock climbing</p>",
+        type: 'attraction'    
+        },
+        {
+        position:{lat:49.056417, lng:20.306968}, // Indoor minigolf
+        content:"<img src='assets/images/info_windows/minigolf_120x100.jpg'><p>Indoor minigolg</p>",
+        type: 'attraction'    
+        },
+        {
+        position:{lat:49.055225, lng:20.307265}, // CORK WINE BAR
+        content:"<img src='assets/images/info_windows/corkwine_120x100.jpg'><p>CORK WINE BAR</p>",
+        type: 'bar'    
+        },
+        {
+        position:{lat:49.054660, lng:20.300495}, // JM Cocktail Bar
+        content:"<img src='assets/images/info_windows/jmcoctail_120x100.jpg'><p>JM Cocktail Bar</p>",
+        type: 'bar'    
+        },
     ];
 
     //Loops through the above array of locations
@@ -65,7 +148,10 @@ function initMap() {
             content: props.content,
         });
         marker.addListener("click", function () {
-        infoWindow.open(mymap, marker);
-        });
+            infoWindow.open(mymap, marker);
+            });
+        }
     };
-}    
+
+
+
